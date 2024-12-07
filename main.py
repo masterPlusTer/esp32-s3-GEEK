5fdcae419e5d2598210f44c6648d4b7acb0bbc6b
+Ter <7396589+masterPlusTer@users.noreply.github.com>
2024-12-07 13:04:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from GEEK import LCD_1inch14
 ### codigo para probar lo que hay hasta ahora, descomentar la linea de codigo para probar el funcionamiento
 
 
+
 lcd = LCD_1inch14()
 #lcd.fill_color(0b1111100000000000)  # Rojo
 #lcd.fill_color(0b0000011111100000)  # verde
@@ -37,11 +38,13 @@ lcd.draw_triangle(50, 50, 150, 50, 100, 100, 0b1111100000000000)  # rojo
 #lcd.draw_trapezoid(50, 50, 150, 50, 120, 100, 80, 100, 0x001F, fill=True)  # Trapecio azul lleno
 #lcd.draw_trapezoid(50, 50, 150, 50, 120, 100, 80, 100, 0x001F)  # Trapecio azul solo borde
 
+########## TEXTO ! U.U ######################33
+
+lcd.draw_text(50, 50, "ESTO ES", 0x07E0)  # Texto verde
+lcd.draw_text(120, 70, "UNA PORQUERIA", 0x001F)  # Texto azul
 
 lcd.show()
 
 
 
 
-
-lcd.show()
