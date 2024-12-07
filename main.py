0f6e5e326eb169975e95ffd75e2733acee341dbb
+Ter <7396589+masterPlusTer@users.noreply.github.com>
2024-12-07 12:10:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,39 @@ lcd.draw_pixel(20, 20, 0b0000011111100000)  # Verde
 lcd.draw_pixel(30, 30, 0b0000000000011111)  # Azul
 
 # Actualizar la pantalla para reflejar los cambios
+
+
+
+#lcd.fill_color(0b1111100000000000)  # Rojo
+#lcd.fill_color(0b0000011111100000)  # verde
+#lcd.fill_color(0xfff) 
+
+#lcd.draw_line(10, 10, 10, 100, 0b0000011111100000)  # verde
+
+#lcd.draw_line(10, 10, 10, 100, 0b1111100000000000)  # rojo
+#lcd.draw_line(10, 10, 10, 100, 0b0000000000011111)  # azul
+lcd.draw_line(10, 10, 10, 100, 0b0000011111100000)  # verde
+
+
+lcd.draw_pixel(10, 10, 0b0000011111100000)  # Verde
+
+lcd.draw_circle(120, 70, 30, 0b0000011111100000)  # Dibuja el círculo con centro en (120, 70) y radio 30 píxeles
+
+lcd.draw_square(50, 30, 40, 0x07E0)  # Cuadrado verde
+lcd.draw_square(50, 30, 40, 0x07E0, fill=True)  # Cuadrado verde lleno
+lcd.draw_circle(120, 67, 50, 0xF800, fill=True)  # Círculo rojo lleno
+
+#lcd.draw_triangle(50, 50, 150, 50, 100, 100, 0x001F, fill=True)  # Azul
+lcd.draw_triangle(50, 50, 150, 50, 100, 100, 0x001F)  # Azul
+
+
+#lcd.draw_shape([(60, 60), (120, 50), (180, 60), (150, 100), (90, 100)], 0x001F, fill=True)  # Pentágono azul lleno
+lcd.draw_shape([(60, 60), (120, 50), (180, 60), (150, 100), (90, 100)], 0b000011111100000, fill=True)  # Pentágono verde lleno
+
+
+
+
+
+
+
 lcd.show()
